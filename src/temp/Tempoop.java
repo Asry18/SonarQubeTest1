dc4898a44d23506dc5fe69c69078a87e85026699
Asry18 <asryamaz000@gmail.com>
2026-01-28 18:10:55 +0530

    no issue in intellij

--- src/temp/Tempoop.java
+++ src/temp/Tempoop.java
@@ -1,13 +1,15 @@
 package temp;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Tempoop {
+
+    static Logger logger = Logger.getLogger(Tempoop.class.getName());
+
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-
-            Logger logger = null;
-            logger.info("Iteration: {}");
+            logger.log(Level.INFO, "Iteration: {0}", i);
         }
     }
 }
