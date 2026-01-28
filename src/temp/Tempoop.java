1f72b51794f83c64f48844f7632a4109163c8403
Asry18 <asryamaz000@gmail.com>
2026-01-28 18:00:31 +0530

    nth after setup

---
+++ src/temp/Tempoop.java
@@ -0,0 +1,13 @@
+package temp;
+
+import java.util.logging.Logger;
+
+public class Tempoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            Logger logger = null;
+            logger.info("Iteration: {}");
+        }
+    }
+}
